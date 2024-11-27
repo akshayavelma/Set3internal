6249d4ef3b53dc0737eec1bf4e876aa263b51e9c
akshayavelma <akshayareddy249@email.com>
2024-11-27 11:42:10 +0530

    first commit

--- PRIMENO.java
+++ PRIMENO.java
@@ -1,21 +1,21 @@
-public class prime {
+public class PRIMENO {
     public static void main(String[] args) {
-        int num = 1;
-        boolean isPrime = true; 
+        int num = 29;
+        boolean isPrime = true;
         if (num <= 1) {
             isPrime = false;
         } else {
             for (int i = 2; i <= num / 2; i++) {
                 if (num % i == 0) {
-                    isPrime = false; 
+                    isPrime = false;
                     break;
                 }
             }
         }
         if (isPrime) {
-            System.out.println(num + " is a prime");
+            System.out.println(num + " is a prime number.");
         } else {
-            System.out.println(num + " is not a prime");
+            System.out.println(num + " is not a prime number.");
         }
     }
 }
